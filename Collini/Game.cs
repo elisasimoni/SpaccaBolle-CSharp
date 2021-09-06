c2b7ce17178eea19ec000f9dbdebf932597d09bc
yuricollini36 <43495892+yuricollini36@users.noreply.github.com>
2021-09-06 16:20:08 +0200

    Add files via upload

---
+++ Collini/Game.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace dev.spaccabolle
+{
+   public class Game : Runnable
+    {
+        private Display display;
+        private int width;
+        private int height;
+        public string title;
+        private bool running = false;
+        private Thread thread;
+        private BufferStrategy bs;
+        private Graphics g;
+
+        public State gameState;
+        public State menuState;
+        public State gameOverState;
+
+        private KeyManager keyManager;
+        private MouseManager mouseManager;
+
+        private Handler handler;
+
+        public Game(String title, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.title = title;
+            this.keyManager = new KeyManager();
+            this.mouseManager = new MouseManager();
+        }
+
+        private void Init()
+        {
+            this.display = new Display(this.title, this.width, this.height);
+            this.display.GetFrame().AddKeyListener(this.keyManager);
+            this.display.GetFrame().AddMouseListener(this.mouseManager);
+            this.display.GetFrame().AddMouseMotionListener(this.mouseManager);
+            this.display.GetCanvas().AddMouseListener(this.mouseManager);
+            this.display.GetCanvas().AddMouseMotionListener(this.mouseManager);
+            Assets.Init();
+            this.handler = new Handler(this);
+            this.menuState = new StateMenu(this.handler);
+            this.gameState = new StateGame(this.handler);
+            State.SetState(this.menuState);
+        }
+
+        public void Restart()
+        {
+            Assets.Init();
+            this.gameState = new StateGame(this.handler);
+        }
+
+        private void Tick()
+        {
+            this.keyManager.Tick();
+            if ((State.GetState() != null))
+            {
+                State.GetState().Tick();
+            }
+
+            if (CollectBall.gameOver)
+            {
+                if (KeyManager.restart)
+                {
+                    this.Restart();
+                    CollectBall.gameOver = false;
+                }
+
+            }
+
+            if (StateMenu.home)
+            {
+                this.Restart();
+                StateMenu.home = false;
+            }
+
+        }
+
+        private void Render()
+        {
+            this.bs = this.display.GetCanvas().GetBufferStrategy();
+            if ((this.bs == null))
+            {
+                this.display.GetCanvas().CreateBufferStrategy(3);
+                return;
+            }
+
+            this.g = this.bs.GetDrawGraphics();
+            if ((State.GetState() != null))
+            {
+                State.GetState().Render(this.g);
+            }
+
+            this.bs.Show();
+            this.g.Dispose();
+        }
+
+        public void Run()
+        {
+            this.Init();
+            int fps = 60;
+            double timePerTick = (1000000000 / fps);
+            double delta = 0;
+            long now;
+            long lastTime = System.nanoTime();
+            long timer = 0;
+            int ticks = 0;
+            while (this.running)
+            {
+                now = System.nanoTime();
+                delta = (delta + ((now - lastTime) / timePerTick));
+                timer = (timer + (now - lastTime));
+                lastTime = now;
+                if ((delta >= 1))
+                {
+                    this.Tick();
+                    this.Render();
+                    ticks++;
+                    delta--;
+                }
+
+                if ((timer >= 1000000000))
+                {
+                    ticks = 0;
+                    timer = 0;
+                }
+
+            }
+
+            this.Stop();
+        }
+
+        public KeyManager GetKeyManager()
+        {
+            return this.keyManager;
+        }
+
+        public MouseManager GetMouseManager()
+        {
+            return this.mouseManager;
+        }
+
+        public int GetWidth()
+        {
+            return this.width;
+        }
+
+        public int GetHeight()
+        {
+            return this.height;
+        }
+
+        public void Start()
+        {
+            if (this.running)
+            {
+                return;
+            }
+
+            this.running = true;
+            this.thread = new Thread(this.Start);
+            this.thread.Start();
+        }
+
+        public void Stop()
+        {
+            if (!this.running)
+            {
+                return;
+            }
+
+            this.running = false;
+            try
+            {
+                this.thread.Join();
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ Collini/Score.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev.spaccabolle.score
+{
+    public class Score
+    {
+        public static int tempFlyngPoint = 0;
+
+        private int tempPoint = 0;
+        private int number1;
+        private int number2;
+        private int number3;
+        private int number4;
+
+        public Score(int number1, int number2, int number3, int number4)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+            this.number3 = number3;
+            this.number4 = number4;
+        }
+
+        public void AddPoint(int point, int flyngPoint)
+        {
+            this.tempPoint = (this.tempPoint + (point * 10));
+            tempFlyngPoint = ((int)((tempFlyngPoint + Math.Pow(2, flyngPoint))));
+            this.tempPoint = (this.tempPoint + tempFlyngPoint);
+            for (int i = 0; (i <= 3); i++)
+            {
+                int definitive = (this.tempPoint / Score.Power(10, (3 - i)));
+                
+                switch (i)
+                {
+                    case 0:
+                        this.number1 = definitive;
+                        break;
+                    case 1:
+                        this.number2 = definitive;
+                        break;
+                    case 2:
+                        this.number3 = definitive;
+                        break;
+                    case 3:
+                        this.number4 = definitive;
+                        break;
+                }
+                this.tempPoint = (this.tempPoint - (definitive * Score.Power(10, (3 - i))));
+            }
+
+            flyngPoint = 0;
+        }
+
+        public int GetNumber1()
+        {
+            return this.number1;
+        }
+
+        public int GetNumber2()
+        {
+            return this.number2;
+        }
+
+        public int GetNumber3()
+        {
+            return this.number3;
+        }
+
+        public int GetNumber4()
+        {
+            return this.number4;
+        }
+
+        public static int Power(int U, int V)
+        {
+            int risp = 1;
+
+            for (int i = 1; i <= V; i++)
+            {
+                risp = (risp * U);
+            }
+
+            return risp;
+        }
+
+        public static void SetZero(Score score)
+        {
+            score.number1 = 0;
+            score.number2 = 0;
+            score.number3 = 0;
+            score.number4 = 0;
+        }
+
+        public void Render(Graphics g)
+        {
+            g.drawImage(Assets.numbers[this.number1], 340, 600, 50, 50, null);
+            g.drawImage(Assets.numbers[this.number2], 380, 600, 50, 50, null);
+            g.drawImage(Assets.numbers[this.number3], 420, 600, 50, 50, null);
+            g.drawImage(Assets.numbers[this.number4], 460, 600, 50, 50, null);
+        }
+    }
+}
